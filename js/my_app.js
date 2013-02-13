be17045d4d937da71bc9d1791b3b60bf94ab7b9c
Voznyakovsky Ivan <ivan.voznyakovsky@gmail.com>
2013-02-13 23:21:59 +0200

    initial commit

---
+++ js/my_app.js
@@ -0,0 +1,24 @@
+!function ($) {
+
+  $(function(){
+  
+      //tolltips
+      $("[rel='tooltip']").tooltip();
+
+      $("[rel='relation']").popover({
+	  html: true,
+	  
+	  
+	  });
+	  
+	  
+  
+  
+  });
+
+
+
+  
+}(window.jQuery);
+
+
