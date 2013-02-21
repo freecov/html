446113953c88012033d1c174b5c4691ee92b435d
konstantincherniak <konstantin.cherniak@gmail.com>
2013-02-22 00:12:05 +0200

    Separate search-form

--- _content/address.php
+++ _content/address.php
@@ -78,75 +78,7 @@
 
 	<div class="span9">
 
-		<div class="row-fluid">
-			<div class="span12">
-				<div class="well well-covide">
-
-					<div class="well well-small well-covide-w">
-						<span class="title">Filter by fields</span>
-
-						<div class="btn-group pull-right" data-toggle="buttons-checkbox">
-							<button type="button" class="btn" data-toggle="collapse" data-target="#search_fields"><i class="icon-angle-up"></i></button>
-						</div>
-					</div>
-
-					<div id="search_fields" class="collapse in">
-                        <form>
-                            <div class="well well-small well-covide-w">
-                                <input type="text" class="span12" placeholder="Search">
-                            </div>
-                            <div class="well well-small well-covide-g form-inline controls-row">
-                                <select class="span3">
-                                    <option>Address</option>
-                                    <option>City</option>
-                                    <option>Zip Code</option>
-                                </select>
-                                <input type="text" class="span7" placeholder="Type address">
-                                <button class="btn btn-small pull-right" type="button"><i class="icon-remove"></i>
-                                </button>
-                            </div>
-                            <div class="well well-small well-covide-w form-inline controls-row">
-                                <select class="span3">
-                                    <option>City</option>
-                                    <option>Address</option>
-                                    <option>Zip Code</option>
-                                </select>
-                                <select class="span7">
-                                    <option>Borneveld</option>
-                                    <option>Rotterdam</option>
-                                    <option>Amsterdam</option>
-                                </select>
-                                <button class="btn btn-small pull-right" type="button"><i class="icon-remove"></i>
-                                </button>
-                            </div>
-                            <div class="well well-small well-covide-g form-inline controls-row">
-                                <select class="span3">
-                                    <option>Zip Code</option>
-                                    <option>City</option>
-                                    <option>Address</option>
-                                </select>
-                                <input type="text" class="span7" placeholder="Type zip code">
-                                <button class="btn btn-small pull-right" type="button"><i class="icon-remove"></i>
-                                </button>
-                            </div>
-                            <br>
-                            <a class="search-link" href="#"><i class="icon-plus"></i> Add condition</a>
-                            <a href="#"><i class="icon-reply"></i> Reset conditions</a>
-                            <hr>
-                            <label class="radio inline">
-                                <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-                                all conditions
-                            </label>
-                            <label class="radio inline">
-                                <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                                at least one condition
-                            </label>
-                            <button type="submit" class="btn pull-right"><i class="icon-search"></i> Search</button>
-                        </form>
-					</div>
-				</div>
-			</div>
-		</div>
+		<? include_once "_parts/search-form.php"; ?>
 
 		<div class="row-fluid">
 			<div class="span12">
