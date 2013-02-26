f68e4546883aa8e4ddec486a2fd8567043217934
konstantincherniak <konstantin.cherniak@gmail.com>
2013-02-26 18:16:15 +0200

    Buisness Card Info modal

--- _modals/add_buisness_card_modal.php
+++ _modals/add_buisness_card_modal.php
@@ -10,20 +10,80 @@
 	
 	<div class="tab-content">
 		<div class="tab-pane active" id="tab1">
-<p><b><i class="icon-info-sign"></i> Information</b></p>
+			<p><b><i class="icon-info-sign"></i> Information</b></p>
 		    <form>
-<div class="controls controls-row">
-<input class="span6" type="text" placeholder="Free Field">
-<input class="span6" type="text" placeholder="Given Name">
-</div>
-<div class="controls controls-row">
-<input class="span6" type="text" placeholder="Letterhead">
-<input class="span6" type="text" placeholder="Insertion">
-</div>
-			
+				<div class="controls controls-row">
+					<input class="span6" type="text" placeholder="Free Field">
+					<input class="span6" type="text" placeholder="Given Name">
+				</div>
+				<div class="controls controls-row">
+					<select class="span6">
+						<option>Select letterhead</option>
+						<option>Beste</option>
+						<option>Geachte</option>
+						<option>Dear</option>
+					</select>
+					<input class="span6" type="text" placeholder="Insertion">
+				</div>
+				<div class="controls controls-row">
+					<select class="span6">
+						<option>Select title</option>
+						<option value="1">Dr.</option>
+						<option value="2">Drs.</option>
+						<option value="3">Ing.</option>
+						<option value="4">Ir.</option>
+						<option value="5">Mr.</option>
+						<option value="6">Prof.</option>
+						<option value="7">Prof. Dr.</option>
+						<option value="8">BSc.</option>
+						<option value="9">MSc.</option>
+						<option value="10">Drs. Ing.</option>
+					</select>
+					<input class="span6" type="text" placeholder="Last Name">
+				</div>
+				<div class="controls controls-row">
+					<input class="span2" type="text" placeholder="Initials">
+					<select class="span2">
+						<option>Select sex</option>
+						<option>Male</option>
+						<option>Female</option>
+					</select>
+					<select class="span3">
+						<option>Select commencement</option>
+						<option value="1">Dhr.</option>
+						<option value="2">Mevr.</option>
+						<option value="3">heer/mevrouw</option>
+						<option value="4">---</option>
+						<option value="5">Mr.</option>
+						<option value="6">Mrs.</option>
+						<option value="7">Ms.</option>
+					</select>
+					<select class="span2">
+						<option>Select suffix</option>
+						<option value="1">I</option>
+						<option value="2">II</option>
+						<option value="3">III</option>
+						<option value="4">Jr.</option>
+						<option value="5">Sr.</option>
+					</select>
+					<input class="span3" type="text" placeholder="Birth Date">
+				</div>
+				<div class="controls controls-row">
+					<input class="span6" type="text" placeholder="SSN">
+					<input class="span6" type="text" placeholder="Person Number">
+				</div>
+				<div class="controls controls-row">
+					<label class="checkbox inline">
+					<input type="checkbox" id="inlineCheckbox1" value="option1"> RC businesscard
+					</label>
+					<label class="checkbox inline">
+					<input type="checkbox" id="inlineCheckbox2" value="option2"> Administrative BC
+					</label>
+					<label class="checkbox inline">
+					<input type="checkbox" id="inlineCheckbox3" value="option3"> Complete with data from RCBC
+					</label>
+				</div>
 			</form>
-		
-		
 		</div>
 		<div class="tab-pane" id="tab2">
 			<p>Howdy, I'm in Section 2.</p>
