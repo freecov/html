dde1877991b8b28b70a8f1ff900b726b86ab305f
konstantincherniak <konstantin.cherniak@gmail.com>
2013-02-27 19:02:43 +0200

    Add classification modal

---
+++ _modals/add_classification_modal.php
@@ -0,0 +1,42 @@
+<div class="input-append span12">
+	<input class="span10" id="appendedInputButton" type="text">
+	<button class="btn" type="button"><i class="icon-search"></i> Search</button>
+</div>
+
+	
+<div class="row-fluid">
+	<div class="span5 text-center">
+		<p>Available classifications:</p>
+		<select multiple="multiple" size="15" class="span12" >
+			<option value="2">do not contact</option>
+			<option value="18">no newsletter</option>
+			<option value="21">19</option>
+			<option value="22">20</option>
+			<option value="6">Escalatie</option>
+			<option value="23">import-201211161529</option>
+			<option value="26">import-201211161539</option>
+			<option value="24">LIMES international tax + expat</option>
+			<option value="25">LIMES international vat + customs</option>
+			<optgroup label="CRM" title="CRM">
+			<option value="14">Klant-CRM</option>
+			</optgroup>
+			<optgroup label="Gapps" title="Gapps">
+			<option value="10">Klant-Apps</option>
+			</optgroup>
+		</select>
+	</div>
+
+	<div class="span2 text-center">
+		<div class="btn-group btn-group-vertical classification-button">
+			<button class="btn" type="button"><i class="icon-arrow-right"></i></button>
+			<button class="btn" type="button"><i class="icon-arrow-left"></i></button>
+		</div>
+	</div>
+
+	<div class="span5 text-center">
+		<p>Applied classifications: </p>
+		<select class="span12" size="15" multiple="multiple">
+
+		</select>
+	</div>
+</div>
