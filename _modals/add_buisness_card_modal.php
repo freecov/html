77eea847e1422bbb246c657dbcd8147a75a2808e
konstantincherniak <konstantin.cherniak@gmail.com>
2013-02-28 17:52:31 +0200

    Add fieldset

--- _modals/add_buisness_card_modal.php
+++ _modals/add_buisness_card_modal.php
@@ -10,82 +10,79 @@
 
 	<div class="tab-content">
 		<div class="tab-pane active" id="tab1">
-			<p><b><i class="icon-info-sign"></i> Information</b></p>
 		    <form>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Free Field">
-					<input class="span6" type="text" placeholder="Given Name">
-				</div>
-				<div class="controls controls-row">
-					<select class="span3">
-						<option>Select letterhead</option>
-						<option>Beste</option>
-						<option>Geachte</option>
-						<option>Dear</option>
-					</select>
-					<input class="span3" type="text" placeholder="Insertion">
-					<select class="span2">
-						<option>Select title</option>
-						<option value="1">Dr.</option>
-						<option value="2">Drs.</option>
-						<option value="3">Ing.</option>
-						<option value="4">Ir.</option>
-						<option value="5">Mr.</option>
-						<option value="6">Prof.</option>
-						<option value="7">Prof. Dr.</option>
-						<option value="8">BSc.</option>
-						<option value="9">MSc.</option>
-						<option value="10">Drs. Ing.</option>
-					</select>
-					<input class="span4" type="text" placeholder="Last Name">
-				</div>
-				<div class="controls controls-row">
-
-					<input class="span2" type="text" placeholder="Initials">
-					<select class="span2">
-						<option>Select sex</option>
-						<option>Male</option>
-						<option>Female</option>
-					</select>
-					<select class="span2">
-						<option>Select suffix</option>
-						<option value="1">I</option>
-						<option value="2">II</option>
-						<option value="3">III</option>
-						<option value="4">Jr.</option>
-						<option value="5">Sr.</option>
-					</select>
-					<select class="span3">
-						<option>Select commencement</option>
-						<option value="1">Dhr.</option>
-						<option value="2">Mevr.</option>
-						<option value="3">heer/mevrouw</option>
-						<option value="4">---</option>
-						<option value="5">Mr.</option>
-						<option value="6">Mrs.</option>
-						<option value="7">Ms.</option>
-					</select>
-					<input class="span3" type="text" placeholder="Birth Date">
-				</div>
-				<div class="controls controls-row">
-
-
-				</div>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="SSN">
-					<input class="span6" type="text" placeholder="Person Number">
-				</div>
-				<div class="controls controls-row">
-					<label class="checkbox inline">
-					<input type="checkbox" id="inlineCheckbox1" value="option1"> RC businesscard
-					</label>
-					<label class="checkbox inline">
-					<input type="checkbox" id="inlineCheckbox2" value="option2"> Administrative BC
-					</label>
-					<label class="checkbox inline">
-					<input type="checkbox" id="inlineCheckbox3" value="option3"> Complete with data from RCBC
-					</label>
-				</div>
+				<fieldset>
+					<legend><i class="icon-info-sign"></i> Information</legend>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Free Field">
+						<input class="span6" type="text" placeholder="Given Name">
+					</div>
+					<div class="controls controls-row">
+						<select class="span3">
+							<option>Select letterhead</option>
+							<option>Beste</option>
+							<option>Geachte</option>
+							<option>Dear</option>
+						</select>
+						<input class="span3" type="text" placeholder="Insertion">
+						<select class="span2">
+							<option>Select title</option>
+							<option value="1">Dr.</option>
+							<option value="2">Drs.</option>
+							<option value="3">Ing.</option>
+							<option value="4">Ir.</option>
+							<option value="5">Mr.</option>
+							<option value="6">Prof.</option>
+							<option value="7">Prof. Dr.</option>
+							<option value="8">BSc.</option>
+							<option value="9">MSc.</option>
+							<option value="10">Drs. Ing.</option>
+						</select>
+						<input class="span4" type="text" placeholder="Last Name">
+					</div>
+					<div class="controls controls-row">
+						<input class="span2" type="text" placeholder="Initials">
+						<select class="span2">
+							<option>Select sex</option>
+							<option>Male</option>
+							<option>Female</option>
+						</select>
+						<select class="span2">
+							<option>Select suffix</option>
+							<option value="1">I</option>
+							<option value="2">II</option>
+							<option value="3">III</option>
+							<option value="4">Jr.</option>
+							<option value="5">Sr.</option>
+						</select>
+						<select class="span3">
+							<option>Select commencement</option>
+							<option value="1">Dhr.</option>
+							<option value="2">Mevr.</option>
+							<option value="3">heer/mevrouw</option>
+							<option value="4">---</option>
+							<option value="5">Mr.</option>
+							<option value="6">Mrs.</option>
+							<option value="7">Ms.</option>
+						</select>
+						<input class="span3" type="text" placeholder="Birth Date">
+					</div>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="SSN">
+						<input class="span6" type="text" placeholder="Person Number">
+					</div>
+					<div class="controls controls-row">
+						<label class="checkbox inline">
+						<input type="checkbox" id="inlineCheckbox1" value="option1"> RC businesscard
+						</label>
+						<label class="checkbox inline">
+						<input type="checkbox" id="inlineCheckbox2" value="option2"> Administrative BC
+						</label>
+						<label class="checkbox inline">
+						<input type="checkbox" id="inlineCheckbox3" value="option3"> Complete with data from RCBC
+						</label>
+					</div>
+				</fieldset>
 			</form>
 		</div>
 		<div class="tab-pane" id="tab2">
@@ -260,89 +257,95 @@
 		</div>
 		<div class="tab-pane" id="tab4">
 			<form>
-				<p><b><i class="icon-building text-error"></i> Private address</b></p>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Address">
-					<input class="span6" type="text" placeholder="Address addition">
-				</div>
-				<div class="controls controls-row">
-					<input class="span3" type="text" placeholder="City">
-					<input class="span3" type="text" placeholder="State/Province">
-					<input class="span2" type="text" placeholder="Zip code">
-					<select class="span4">
-						<option>Country</option>
-						<option value="AF">Afghanistan</option>
-						<option value="AL">Albania</option>
-						<option value="DZ">Algeria</option>
-						<option value="AS">American Samoa</option>
-						<option value="AD">Andorra</option>
-						<option value="AO">Angola</option>
-						<option value="AI">Anguilla</option>
-						<option value="AQ">Antarctica</option>
-						<option value="AG">Antigua And Barbuda</option>
-						<option value="AR">Argentina</option>
-						<option value="AM">...</option>
-					</select>
-				</div>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Telephone nr 1">
-					<input class="span6" type="text" placeholder="Telephone nr 2">
-				</div>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Mobile phone nr">
-					<input class="span6" type="text" placeholder="Fax nr">
-				</div>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Email">
-					<input class="span6" type="text" placeholder="Skype">
-				</div>
+				<fieldset>
+                    <legend><i class="icon-building text-error"></i> Private address</legend>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Address">
+						<input class="span6" type="text" placeholder="Address addition">
+					</div>
+					<div class="controls controls-row">
+						<input class="span3" type="text" placeholder="City">
+						<input class="span3" type="text" placeholder="State/Province">
+						<input class="span2" type="text" placeholder="Zip code">
+						<select class="span4">
+							<option>Country</option>
+							<option value="AF">Afghanistan</option>
+							<option value="AL">Albania</option>
+							<option value="DZ">Algeria</option>
+							<option value="AS">American Samoa</option>
+							<option value="AD">Andorra</option>
+							<option value="AO">Angola</option>
+							<option value="AI">Anguilla</option>
+							<option value="AQ">Antarctica</option>
+							<option value="AG">Antigua And Barbuda</option>
+							<option value="AR">Argentina</option>
+							<option value="AM">...</option>
+						</select>
+					</div>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Telephone nr 1">
+						<input class="span6" type="text" placeholder="Telephone nr 2">
+					</div>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Mobile phone nr">
+						<input class="span6" type="text" placeholder="Fax nr">
+					</div>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Email">
+						<input class="span6" type="text" placeholder="Skype">
+					</div>
+				</fieldset>
 			</form>
 		</div>
 		<div class="tab-pane" id="tab5">
 			<form>
-				<p><b><i class="icon-building text-info"></i> Other address</b></p>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Address">
-					<input class="span6" type="text" placeholder="Address addition">
-				</div>
-				<div class="controls controls-row">
-					<input class="span3" type="text" placeholder="City">
-					<input class="span3" type="text" placeholder="State/Province">
-					<input class="span2" type="text" placeholder="Zip code">
-					<select class="span4">
-						<option>Country</option>
-						<option value="AF">Afghanistan</option>
-						<option value="AL">Albania</option>
-						<option value="DZ">Algeria</option>
-						<option value="AS">American Samoa</option>
-						<option value="AD">Andorra</option>
-						<option value="AO">Angola</option>
-						<option value="AI">Anguilla</option>
-						<option value="AQ">Antarctica</option>
-						<option value="AG">Antigua And Barbuda</option>
-						<option value="AR">Argentina</option>
-						<option value="AM">...</option>
-					</select>
-				</div>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Telephone nr 1">
-					<input class="span6" type="text" placeholder="Telephone nr 2">
-				</div>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Mobile phone nr">
-					<input class="span6" type="text" placeholder="Fax nr">
-				</div>
-				<div class="controls controls-row">
-					<input class="span6" type="text" placeholder="Email">
-				</div>
+				<fieldset>
+                    <legend><i class="icon-building text-info"></i> Other address</legend>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Address">
+						<input class="span6" type="text" placeholder="Address addition">
+					</div>
+					<div class="controls controls-row">
+						<input class="span3" type="text" placeholder="City">
+						<input class="span3" type="text" placeholder="State/Province">
+						<input class="span2" type="text" placeholder="Zip code">
+						<select class="span4">
+							<option>Country</option>
+							<option value="AF">Afghanistan</option>
+							<option value="AL">Albania</option>
+							<option value="DZ">Algeria</option>
+							<option value="AS">American Samoa</option>
+							<option value="AD">Andorra</option>
+							<option value="AO">Angola</option>
+							<option value="AI">Anguilla</option>
+							<option value="AQ">Antarctica</option>
+							<option value="AG">Antigua And Barbuda</option>
+							<option value="AR">Argentina</option>
+							<option value="AM">...</option>
+						</select>
+					</div>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Telephone nr 1">
+						<input class="span6" type="text" placeholder="Telephone nr 2">
+					</div>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Mobile phone nr">
+						<input class="span6" type="text" placeholder="Fax nr">
+					</div>
+					<div class="controls controls-row">
+						<input class="span6" type="text" placeholder="Email">
+					</div>
+				</fieldset>
 			</form>
 		</div>
 		<div class="tab-pane" id="tab6">
 			<form>
-				<p><b><i class="icon-info-sign"></i> Other</b></p>
-				<div class="controls controls-row">
-					<textarea  class="span12" rows="3"></textarea>
-				</div>
+				<fieldset>
+                    <legend><i class="icon-info-sign"></i> Other</legend>
+					<div class="controls controls-row">
+						<textarea  class="span12" rows="3"></textarea>
+					</div>
+				</fieldset>
 			</form>
 		</div>
 	</div>
