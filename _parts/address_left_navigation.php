219f815d1e689dabedda2f882f3833056bc3ef8a
lockheedman <lockheedman@gmail.com>
2013-03-05 13:03:08 +0200

    remove unneeded classes

--- _parts/address_left_navigation.php
+++ _parts/address_left_navigation.php
@@ -54,7 +54,7 @@
 		<!-- End Person Modal -->
 
 		<hr>
-		<div class="well well-covide-menu">
+		<div class="well">
 			<ul class="nav nav-tabs nav-stacked nav-covide">
 				<li>
                     <a data-toggle="collapse" data-target="#submenu1" href="#"><i class="icon-sitemap"></i> Relations</a>
--- _parts/email_left_navigation.php
+++ _parts/email_left_navigation.php
@@ -43,7 +43,7 @@
 		<!-- End Quick Email Modal -->
 
 		<hr>
-		<div class="well well-covide-menu">
+		<div class="well">
 			<h4>Email folders</h4>
 		    <div class="tabbable"> <!-- Only required for left/right tabs -->
 				<ul class="nav nav-tabs">
