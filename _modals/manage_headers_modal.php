20cd9507babb6987c1e258449de5559672ce9a2f
konstantincherniak <konstantin.cherniak@gmail.com>
2013-04-29 17:50:00 +0300

    change headers modal

--- _modals/manage_headers_modal.php
+++ _modals/manage_headers_modal.php
@@ -1,49 +1,288 @@
 	<div class="tabbable">
-		<ul class="nav nav-pills">
+		<ul class="nav nav-tabs">
 			<li class="active">
-				<a href="#step1_ivm" data-toggle="tab">Step 1</a>
+				<a href="#Title_mhm" data-toggle="tab">Title</a>
 			</li>
 			<li>
-				<a href="#step2_ivm" data-toggle="tab">Step 2</a>
+				<a href="#Commencement_mhm" data-toggle="tab">Commencement</a>
+			</li>
+			<li>
+				<a href="#Letterhead_mhm" data-toggle="tab">Letterhead</a>
+			</li>
+			<li>
+				<a href="#Suffix_mhm" data-toggle="tab">Suffix</a>
 			</li>
 		</ul>
 		<div class="tab-content">
-			<div class="tab-pane active" id="step1_ivm">
-				<div class="fieldset-group" id="tab_links_asim">
-					<form>
-						<div class="row-fluid">
-							<div class="span12 form-inline controls-row">
-								<fieldset>
-									<legend><i class="icon-copy"></i> Choose file to import (VCF format)</legend>
-									<ul class="list-striped linked-ul inline">
-										<li class="odd form-inline controls-row">Attach.docx <button type="button" class="close close-extra-field"><i class="icon-remove-sign"></i></button></li>
-									</ul>
-									<button class="btn" type="button"> Choose file</button>
-								</fieldset>
-							</div>
-						</div>
-					</form>
-				</div>
+			<div class="tab-pane active" id="Title_mhm">
+				
+				<div class="well well-covide">
+					<div class="well-title">
+            		    <span class="title">Overview of headers</span>
+            	    </div>
+	                
+        	        <table class="table table-condensed table-striped table-hover">
+	                    <colgroup>
+        	                <col>
+                	        <col width=15%>
+                	    </colgroup>
+                	    <thead>
+                    	    <tr>
+                        	    <th>Title</th>
+                        	    <th></th>
+                    	    </tr>
+                	    </thead>
+                	    <tbody>
+                    	    <tr>
+                        	    <td>Dr.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addtitle_mhmModal" data-target="#addtitle_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>Drs.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addtitle_mhmModal" data-target="#addtitle_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>Ing.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addtitle_mhmModal" data-target="#addtitle_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                	    </tbody>
+            	    </table>
+        	    </div>
+				
+				<a class="btn btn-warning" type="button" data-toggle="modal" href="#addtitle_mhmModal" data-target="#addtitle_mhmModal" ><i class="icon-plus-sign"></i> Add new title</a>
+                       
+				<!-- Start Add new title Modal -->
+                <div id="addtitle_mhmModal" class="modal add-classification-modal hide fade text-left" tabindex="-1" role="dialog" aria-labelledby="addtitle_mhmModalLabel" aria-hidden="true">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-remove-sign"></i></button>
+						<h4 id="addtitle_mhmModalLabel">Add new title</h4>
+                    </div>
+                    <div class="modal-body">
+						<input type="text" placeholder="Title">
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                        <button class="btn btn-warning">Add new title</button>
+                    </div>
+                </div>
+                <!-- End Add new title Modal -->
 			</div>
-			<div class="tab-pane" id="step2_ivm">
-				<div class="fieldset-group" id="tab_links_asim">
-					<form>
-						<div class="row-fluid">
-							<div class="span12 form-inline controls-row">
-								<fieldset>
-									<legend><i class="icon-book"></i> Contacts</legend>
-									<ul class="list-striped linked-ul inline">
-										<li class="odd form-inline controls-row">Klant-Apps <button type="button" class="close close-extra-field" ><i class="icon-remove-sign"></i></button></li>
-										<li class="form-inline controls-row">Klant-CRM <button type="button" class="close close-extra-field" ><i class="icon-remove-sign"></i></button></li>
-									</ul>
-									<div class="controls controls-row">
-										<button class="btn span2" type="button" data-toggle="modal" href="_modals/add_contact_classification_modal.php" data-target="#addcontactModal" ><i class="icon-plus"></i> Add</button>
-									</div>
-								</fieldset>
-							</div>
-						</div>
-					</form>
-				</div>
+
+
+			<div class="tab-pane" id="Commencement_mhm">
+				
+				<div class="well well-covide">
+					<div class="well-title">
+            		    <span class="title">Overview of headers</span>
+            	    </div>
+	                
+        	        <table class="table table-condensed table-striped table-hover">
+	                    <colgroup>
+        	                <col>
+                	        <col width=15%>
+                	    </colgroup>
+                	    <thead>
+                    	    <tr>
+                        	    <th>Commencement</th>
+                        	    <th></th>
+                    	    </tr>
+                	    </thead>
+                	    <tbody>
+                    	    <tr>
+                        	    <td>Dhr.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addcommencement_mhmModal" data-target="#addcommencement_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>Mevr.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addcommencement_mhmModal" data-target="#addcommencement_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>heer/mevrouw.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addcommencement_mhmModal" data-target="#addcommencement_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                	    </tbody>
+            	    </table>
+        	    </div>
+				
+				<a class="btn btn-warning" type="button" data-toggle="modal" href="#addcommencement_mhmModal" data-target="#addcommencement_mhmModal" ><i class="icon-plus-sign"></i> Add new commencement</a>
+                       
+				<!-- Start Add new commencement Modal -->
+                <div id="addcommencement_mhmModal" class="modal add-classification-modal hide fade text-left" tabindex="-1" role="dialog" aria-labelledby="addcommencement_mhmModalLabel" aria-hidden="true">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-remove-sign"></i></button>
+						<h4 id="addcommencement_mhmModalLabel">Add new commencement</h4>
+                    </div>
+                    <div class="modal-body">
+						<input type="text" placeholder="Commencement">
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                        <button class="btn btn-warning">Add new commencement</button>
+                    </div>
+                </div>
+                <!-- End Add new commencement Modal -->
+				
+				
+			</div>
+			
+			<div class="tab-pane" id="Letterhead_mhm">
+				
+				<div class="well well-covide">
+					<div class="well-title">
+            		    <span class="title">Overview of headers</span>
+            	    </div>
+	                
+        	        <table class="table table-condensed table-striped table-hover">
+	                    <colgroup>
+        	                <col>
+                	        <col width=15%>
+                	    </colgroup>
+                	    <thead>
+                    	    <tr>
+                        	    <th>Letterhead</th>
+                        	    <th></th>
+                    	    </tr>
+                	    </thead>
+                	    <tbody>
+                    	    <tr>
+                        	    <td>Dhr.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addLetterhead_mhmModal" data-target="#addLetterhead_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>Mevr.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addLetterhead_mhmModal" data-target="#addLetterhead_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>heer/mevrouw.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addLetterhead_mhmModal" data-target="#addLetterhead_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                	    </tbody>
+            	    </table>
+        	    </div>
+				
+				<a class="btn btn-warning" type="button" data-toggle="modal" href="#addLetterhead_mhmModal" data-target="#addLetterhead_mhmModal" ><i class="icon-plus-sign"></i> Add new Letterhead</a>
+                       
+				<!-- Start Add new Letterhead Modal -->
+                <div id="addLetterhead_mhmModal" class="modal add-classification-modal hide fade text-left" tabindex="-1" role="dialog" aria-labelledby="addLetterhead_mhmModalLabel" aria-hidden="true">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-remove-sign"></i></button>
+						<h4 id="addLetterhead_mhmModalLabel">Add new Letterhead</h4>
+                    </div>
+                    <div class="modal-body">
+						<input type="text" placeholder="Letterhead">
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                        <button class="btn btn-warning">Add new Letterhead</button>
+                    </div>
+                </div>
+                <!-- End Add new Letterhead Modal -->
+				
+				
+			</div>
+			
+			<div class="tab-pane" id="Suffix_mhm">
+				
+				<div class="well well-covide">
+					<div class="well-title">
+            		    <span class="title">Overview of headers</span>
+            	    </div>
+	                
+        	        <table class="table table-condensed table-striped table-hover">
+	                    <colgroup>
+        	                <col>
+                	        <col width=15%>
+                	    </colgroup>
+                	    <thead>
+                    	    <tr>
+                        	    <th>Suffix</th>
+                        	    <th></th>
+                    	    </tr>
+                	    </thead>
+                	    <tbody>
+                    	    <tr>
+                        	    <td>Dhr.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addSuffix_mhmModal" data-target="#addSuffix_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>Mevr.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addSuffix_mhmModal" data-target="#addSuffix_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                    	    <tr>
+                        	    <td>heer/mevrouw.</td>
+                        	    <td>
+								    <div class="btn-group">
+									    <a class="btn btn-small" type="button" data-toggle="modal" href="#addSuffix_mhmModal" data-target="#addSuffix_mhmModal" title="Edit"><i class="icon-edit"></i></a> <a class="btn btn-danger btn-small" href="#" title="Delete"><i class="icon-trash"></i></a>
+								    </div>
+							    </td>
+                    	    </tr>
+                	    </tbody>
+            	    </table>
+        	    </div>
+				
+				<a class="btn btn-warning" type="button" data-toggle="modal" href="#addSuffix_mhmModal" data-target="#addSuffix_mhmModal" ><i class="icon-plus-sign"></i> Add new Suffix</a>
+                       
+				<!-- Start Add new Suffix Modal -->
+                <div id="addSuffix_mhmModal" class="modal add-classification-modal hide fade text-left" tabindex="-1" role="dialog" aria-labelledby="addSuffix_mhmModalLabel" aria-hidden="true">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="icon-remove-sign"></i></button>
+						<h4 id="addSuffix_mhmModalLabel">Add new Suffix</h4>
+                    </div>
+                    <div class="modal-body">
+						<input type="text" placeholder="Suffix">
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                        <button class="btn btn-warning">Add new Suffix</button>
+                    </div>
+                </div>
+                <!-- End Add new Suffix Modal -->
+				
+				
 			</div>
 		</div>
 	</div>
